47921367071625b1a68a8da29039de2d4575bd60
daltonfranco <daltonpacolafranco.git@hotmail.com>
2022-04-05 16:52:42 +0000

    exercicio1 da p1 de java2

---
+++ Funcionario.java
@@ -0,0 +1,39 @@
+public class Funcionario{
+
+    private String nome;
+    private String cpf;
+    private double salario;
+    private String departamento;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public String getDepartamento() {
+        return departamento;
+    }
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public double bonificacao(){
+        return 1200.0;
+    }
+
+    
+
+}
---
+++ Principal.java
@@ -0,0 +1,40 @@
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+        Gerente g = new Gerente();
+
+        g.setNome("Dalton");
+        g.setCpf("12312312345");
+        g.setSalario(1200.0);
+        g.setDepartamento("Tecnologia");
+        g.setSenha(12332);
+
+        System.out.println();
+
+        System.out.println(g.getNome());
+        System.out.println(g.getCpf());
+        System.out.println(g.getSalario());
+        System.out.println(g.getDepartamento());
+        System.out.println(g.bonificacao());
+        System.out.println(g.autentificacao());
+
+
+        System.out.println();
+
+        Funcionario f = new Funcionario();
+
+        f.setNome("Roberto");
+        f.setCpf("56756756789");
+        f.setSalario(6200.0);
+        f.setDepartamento("Administracao");
+
+        System.out.println(f.getNome());
+        System.out.println(f.getCpf());
+        System.out.println(f.getSalario());
+        System.out.println(f.getDepartamento());
+        System.out.println(f.bonificacao());
+
+    }
+
+}
